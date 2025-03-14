773d5241c8db358ca91a495f70ed163273a81eff
JosefinaRios <josefinarios857@gmail.com>
2025-03-14 19:34:21 -0300

    subiendo conexion sqlite

--- Database/Conection.py
+++ Database/Conection.py
@@ -1,27 +1,41 @@
-import mysql.connector
+import sqlitecloud
 
-conection = mysql.connector.connect(
-    host="localhost",  # Servidor
-    user="root",       # Usuario de MySQL
-    password="SpayCliforTom45",  # Reemplázalo con tu contraseña
-    database="agenda_digital",
-      port="3306"  # Reemplázalo con tu base de datos
-)
+try:
+    conn = sqlitecloud.connect("sqlitecloud://cohizet2hz.g5.sqlite.cloud:8860/AgendaDigital?apikey=B97PQqmdOvaTZsNbiDnv6gPLX76XQlRUBkZ4b9t84ew")
+    print("Conexión exitosa a la base de datos")
+    conn.close()
+except Exception as e:
+    print(f"Error de conexión: {e}")
+# try:
+# conn = sqlitecloud.connect("sqlitecloud://cohizet2hz.g5.sqlite.cloud:8860/AgendaDigital?apikey=B97PQqmdOvaTZsNbiDnv6gPLX76XQlRUBkZ4b9t84ew")
+# cursor = conn.execute('SELECT * FROM Users;')
+# result = cursor.fetchone()
 
-if conection.is_connected():
-    print("Conexión exitosa a MySQL")
+# print(result)
 
-cursor = conection.cursor()
+# conn.close()
+# conection = mysql.connector.connect(
+#     host="localhost",  # Servidor
+#     user="root",       # Usuario de MySQL
+#     password="SpayCliforTom45",  # Reemplázalo con tu contraseña
+#     database="agenda_digital",
+#       port="3306"  # Reemplázalo con tu base de datos
+# )
 
-cursor.execute("INSERT INTO Users (Nombre, Email, Contraseña) VALUES  ('Cele', 'cele@example.com', 'k8sw34rt')")
-conection.commit()
+# if conection.is_connected():
+#     print("Conexión exitosa a MySQL")
 
-# Leer datos
-cursor.execute("SELECT * FROM Users")
-for usuario in cursor.fetchall():
-    print(usuario)
+# cursor = conection.cursor()
 
-# Cerrar conexión
-cursor.close()
-# Cerrar conexión
-conection.close()
+# cursor.execute("INSERT INTO Users (Nombre, Email, Contraseña) VALUES  ('Cele', 'cele@example.com', 'k8sw34rt')")
+# conection.commit()
+
+# # Leer datos
+# cursor.execute("SELECT * FROM Users")
+# for usuario in cursor.fetchall():
+#     print(usuario)
+
+# # Cerrar conexión
+# cursor.close()
+# # Cerrar conexión
+# conection.close()
